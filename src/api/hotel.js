48ffec7e7afe896273aa05eb102e47d8cbd38618
Jinyu Zhu <jinyuzhu@smail.nju.edu.cn>
2020-05-23 10:02:14 +0800

    feat: search refine, no period search

--- src/api/hotel.js
+++ src/api/hotel.js
@@ -23,13 +23,12 @@ export function managedOrdersAPI(id) {
 }
 
 //发送酒店搜索的请求
-export function submitHotelSearchParamsAPI(params){
+export function submitHotelSearchParamsAPI(data){
     console.log("------")
-    console.log(params)
     return axios({
         url: `${api.hotelPre}/hotel_search/detail`,
         method: 'POST',
-        params,
+        data,
     })
 }
 //不知道为啥，拦截不了这里的axios默认发8080干
--- src/views/hotel/components/hotelListForSearch.vue
+++ src/views/hotel/components/hotelListForSearch.vue
@@ -1,5 +1,5 @@
 <template>
-    <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
+    <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="searchedHotelList">
         <div slot="footer">from <b>NJU-SE</b> hotel search</div>
         <a-list-item slot="renderItem" key="item.title" slot-scope="item">
             <template slot="actions">
@@ -8,11 +8,11 @@
                       {{ item.hotelStar }}
                     </span>
                 <span :key="like-o">
-                      <a-icon type="like-o" style="margin-right: 8px" />
+                      <a-icon type="like-o" style="margin-right: 8px"/>
                       {{ item.rate }}
                     </span>
                 <span :key="tag" v-if="item.isOrdered">
-                      <a-icon type="tag" style="margin-right: 8px" />
+                      <a-icon type="tag" style="margin-right: 8px"/>
                        "曾经预定过"
                 </span>
 
@@ -26,34 +26,19 @@
                     alt="logo"
                     :src="item.hotelPic"
             />
-<!--            先限制一下，等到之后再处理拉伸变形问题-->
-            <a-list-item-meta :description="'地址:'+item.address+' 商圈:'+item.biz_id">
+            <!--            先限制一下，等到之后再处理拉伸变形问题-->
+            <a-list-item-meta :description="item.address">
                 <a slot="title" :href="item.href">{{ item.name }}</a>
                 <!--                <a-avatar slot="avatar" :src="item.avatar"/>-->
             </a-list-item-meta>
             {{ item.description }}
-<!--            <h4 >{{item.lowestprice}}</h4>-->
+            <!--            <h4 >{{item.lowestprice}}</h4>-->
         </a-list-item>
     </a-list>
 </template>
 <script>
-    const listData = [];
-    for (let i = 0; i < 23; i++) {
-        listData.push({
-            id: 1,
-            name: `酒店名称 酒店${i}`,
-            address: '酒店地址',
-            biz_id:'商圈所在',
-            hotelStar: 5,
-            rate: 4.7,
-            description:
-                '酒店的简介，服务，等介绍',
-            phoneNum:"13333217870",
-            hotelPic: 'https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png',//目前没有
-            lowestprice:200,//目前没有
-            isOrdered: true,//目前没有
-        });
-    }
+    import {mapGetters, mapMutations, mapActions} from 'vuex'
+
     //Integer id;
     //  String name;
     // String address;
@@ -64,9 +49,11 @@
     // String phoneNum;
     //Integer managerId;
     export default {
+        computed: {
+            ...mapGetters(['searchedHotelList'])
+        },
         data() {
             return {
-                listData,
                 pagination: {
                     onChange: page => {
                         console.log(page);
